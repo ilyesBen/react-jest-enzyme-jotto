6ea7d7645c8af7200dbac7e83645e6a3794ace4b
Ilyes Ben <ilyes.benmansour@ideadrop.co>
2020-01-03 17:39:01 +0000

    Added basic test to App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const setup = () => {
+  return shallow(<App />);
+};
+
+test("renders App component without error", () => {
+  const wrapper = setup();
+  expect(wrapper.exists()).toBeTruthy();
 });
