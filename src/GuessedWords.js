41c68829b23d5f9f82de1b7c35e610432eaa0052
Ilyes Ben <ilyes.benmansour@ideadrop.co>
2020-01-03 17:17:40 +0000

    GuessedWord component created. test added for GuessedWord

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -3,9 +3,30 @@ import PropTypes from "prop-types";
 
 const GuessedWords = props => {
   const { guessedWords } = props;
-  const contents = guessedWords.length === 0 && (
-    <span data-test="guess-instructions">Try to guess the secret word!</span>
-  );
+  const contents =
+    guessedWords.length === 0 ? (
+      <span data-test="guess-instructions">Try to guess the secret word!</span>
+    ) : (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+        </table>
+        <tbody>
+          {guessedWords.map((word, index) => (
+            <tr data-test="guessed-word" key={index}>
+              <td>{word.guessedWord}</td>
+              <td>{word.letterMatchCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </div>
+    );
 
   return <div data-test="component-guessed-word">{contents}</div>;
 };
