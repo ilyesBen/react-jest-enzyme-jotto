229df7eee7548ea54493674ec39ecee57d77ee80
Ilyes Ben <ilyes.benmansour@ideadrop.co>
2020-01-03 17:36:46 +0000

    bootstrap styling added. App test removed

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -9,22 +9,22 @@ const GuessedWords = props => {
     ) : (
       <div data-test="guessed-words">
         <h3>Guessed Words</h3>
-        <table>
-          <thead>
+        <table className="table table-sm">
+          <thead className="thead-light">
             <tr>
               <th>Guess</th>
               <th>Matching Letters</th>
             </tr>
           </thead>
+          <tbody>
+            {guessedWords.map((word, index) => (
+              <tr data-test="guessed-word" key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        <tbody>
-          {guessedWords.map((word, index) => (
-            <tr data-test="guessed-word" key={index}>
-              <td>{word.guessedWord}</td>
-              <td>{word.letterMatchCount}</td>
-            </tr>
-          ))}
-        </tbody>
       </div>
     );
 
